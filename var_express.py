f96e96d75f5ad8faf9c66a515b5039042bee11e5
Manuel Henke <manuel.henke@gmail.com>
2023-03-14 14:02:46 +0100

    add operatoren

---
+++ var_express.py
@@ -0,0 +1,29 @@
+
+zahl1 = 220
+zahl2 = 11
+zahl3 = 8
+text1 = "12"
+text2 = "Hello"
+text3 = "Welt"
+
+
+#88
+print(zahl2 * zahl3)
+#20
+print(zahl3 + int(text1))
+#Welt*11
+print(zahl2 * text3)
+#  4
+print(zahl1 % zahl3)
+#Hallo schöne Welt
+print(text2 + ' schöne ' + text3)
+#true
+print(zahl2 <= (zahl3 + 2 + zahl2))
+#false
+print(text2 == text3)
+#
+print(zahl2 & zahl3)
+
+#
+print('{0:08b}'.format(zahl2))
+print('{0:08b}'.format(zahl3))

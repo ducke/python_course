7fcb0d6b46aeb7ca51aee91620b3c3c693d9ae9f
Manuel Henke <manuel.henke@gmail.com>
2023-03-14 11:36:39 +0100

    add doc string

--- erste.py
+++ erste.py
@@ -3,9 +3,14 @@
 # aktuelles Datum als string
 aktuellesDatum = "14.03.2023"
 # Die Ausgabe
-# print("Das aktuelle Datum:\n", aktuellesDatum, '''
-# Lorem ipsum dolor sit amet, consetetur\n  sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita\nkasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.''')
+print("Das aktuelle Datum:\n", aktuellesDatum, '''
+Lorem ipsum dolor sit amet, consetetur\n  sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita\nkasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.''')
 
-print("Aktuelles Datum:")
-print(aktuellesDatum)
 
+# docstring
+'''
+Author:
+Datum:
+Version:
+
+'''
